0a920c2018b22b2c6f98333c34c99b63ad9c2fc2
farneser <farneser1@gmail.com>
2023-08-18 04:26:39 +0300

    feat: resources, http requests

---
+++ src/main/resources/tables.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Currencies
+(
+    ID       INTEGER PRIMARY KEY AUTOINCREMENT,
+    Code     VARCHAR(30) NOT NULL,
+    FullName VARCHAR(30) NOT NULL,
+    Sign     VARCHAR(30) NOT NULL,
+    UNIQUE (Code)
+);
+
+
+CREATE TABLE ExchangeRates
+(
+    ID               INTEGER PRIMARY KEY AUTOINCREMENT,
+    BaseCurrencyId   INTEGER,
+    TargetCurrencyId INTEGER,
+    Rate             BIGINT(20, 6) NOT NULL,
+    FOREIGN KEY (BaseCurrencyId) REFERENCES Currencies (ID),
+    FOREIGN KEY (TargetCurrencyId) REFERENCES Currencies (ID),
+    UNIQUE (BaseCurrencyId, TargetCurrencyId)
+);
