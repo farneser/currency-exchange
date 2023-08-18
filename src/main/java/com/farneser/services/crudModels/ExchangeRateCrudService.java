7db829bce8ebe0c5d39d5be2145214c7fad08a7f
farneser <farneser1@gmail.com>
2023-08-18 19:46:32 +0300

    fix: ExchangeRateCrudService implement ICrud interface

--- src/main/java/com/farneser/services/crudModels/ExchangeRateCrudService.java
+++ src/main/java/com/farneser/services/crudModels/ExchangeRateCrudService.java
@@ -4,10 +4,36 @@ import com.farneser.models.ExchangeRate;
 import com.farneser.services.CrudService;
 
 import java.sql.Connection;
+import java.util.List;
 
 public class ExchangeRateCrudService extends CrudService<ExchangeRate> {
 
     public ExchangeRateCrudService(Connection connection) {
         super(connection, "ExchangeRates");
+    }
+
+    @Override
+    public void create(ExchangeRate obj) {
+
+    }
+
+    @Override
+    public void update(ExchangeRate obj) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<ExchangeRate> get() {
+        return null;
+    }
+
+    @Override
+    public ExchangeRate get(int id) {
+        return null;
     }
 }
