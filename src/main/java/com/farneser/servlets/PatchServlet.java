04ab2b9c02266e3b54b4610c1c1a589b6e255107
farneser <farneser1@gmail.com>
2023-08-20 15:09:58 +0300

    feat: patch servlet

---
+++ src/main/java/com/farneser/servlets/PatchServlet.java
@@ -0,0 +1,19 @@
+package com.farneser.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public abstract class PatchServlet extends BaseServlet {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getMethod().equalsIgnoreCase("PATCH")){
+            doPatch(request, response);
+        } else {
+            super.service(request, response);
+        }
+    }
+
+    protected abstract void doPatch(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
